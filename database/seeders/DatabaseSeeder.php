2df7b282c72a6f34dda704eaf1af41838a60030a
bighe <infobighenrique@gmail.com>
2025-10-09 23:54:51 -0300

    atividade-2:feat: da DataseSeeder.ph

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as FakerFactory;
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+    public function run()
+    {
+        FakerFactory::create()->unique(true);
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
         ]);
     }
 }
