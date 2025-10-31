9791b7b9ed5a0b7ba20a1283cb7d3bba927e3401
bigg <infobighenrique225@gmail.com>
2025-10-30 23:20:12 -0300

    feat: add category controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('categories', CategoryController::class);
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
