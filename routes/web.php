9d5230804d4faecf9eb163d42cafcb2fb01104a9
Gabriel Henrique Alves Da Silva <infobighenrique@gmail.com>
2025-10-31 15:28:05 -0300

    adição das rotas de books no web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authorController;
 use App\Http\Controllers\CategoryController;
@@ -8,8 +9,15 @@ use App\Http\Controllers\PublisherController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+// Rotas para criação de livros
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
 
 
+// Rotas RESTful para index, show, edit, update, delete (tem que ficar depois das rotas /books/create-id-number e /books/create-select)
+Route::resource('books', BookController::class)->except(['create', 'store']);
 Route::resource('author', authorController::class);
 Route::resource('publisher', PublisherController::class);
 Route::resource('categories', CategoryController::class);
